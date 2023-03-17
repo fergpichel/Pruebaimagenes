2a182e7da5c1fe0758096c2794790778abf3a5be
Alfredo <minosabr@hotmail.com>
2023-03-17 20:27:06 +0100

    Se inicia el proyecto para tratar de que se muestren las imagenes en android

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,17 +1,23 @@
 <template>
     <Page>
         <ActionBar>
-            <Label text="Home"/>
+            <Label text="Prueba chunga"/>
         </ActionBar>
+        <ScrollView>
+              <StackLayout>
+                  <Label text="Esto no va 01" />
+                  <Image src="app/assets/00prueba.png"/>
+                  <Image src="~/assets/01Longitud.png" stretch="none" />
+                  <Button text="Botón" @tap="onButtonTap" />
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
+                  <Image src="~/assets/01Longitud.png" stretch="none" />
+                  <Image src="~/assets/00prueba.png" stretch="none" />
+                  <Image src="https://i.etsystatic.com/6792824/r/il/4cd9bd/4246801182/il_794xN.4246801182_d2z8.jpg" />
+                  <Label text="Esto no va 02" />
+                  <Image src="~/assets/00prueba.png" stretch="none" />
+                  <Image src="https://i.etsystatic.com/6792824/r/il/4cd9bd/4246801182/il_794xN.4246801182_d2z8.jpg" stretch="none" />
+              </StackLayout>
+        </ScrollView>
     </Page>
 </template>
 
@@ -26,16 +32,16 @@
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+    //@import '@nativescript/theme/scss/variables/blue';
 
     // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+    //.fas {
+       // @include colorize($color: accent);
+    //}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+    //.info {
+        //font-size: 20;
+        //horizontal-align: center;
+        //vertical-align: center;
+    //}
 </style>
