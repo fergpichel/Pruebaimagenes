9db0cee12bdcae0f4b79a7704f8c458cb969bc25
Alfredo <minosabr@hotmail.com>
2023-03-19 09:14:25 +0100

    Se añaden 10 imágenes cargadas de forma diferente

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,27 +1,48 @@
 <template>
     <Page>
         <ActionBar>
-            <Label text="Prueba chunga"/>
+            <Label text="Prueba chunga 7"/>
         </ActionBar>
         <ScrollView>
               <StackLayout>
-                  <Label text="Esto no va 01" />
-                  <Image src="app/assets/00prueba.png"/>
+                  <Label text="Imagen 1" />
+                  <Image src="~/assets/00prueba"/>
+
+                  <Label text="Imagen 2" />
+                  <Image src="~/01Longitud"/>
+
+                  <Label text="Imagen 3" />
+                  <Image src="@/assets/00prueba.png"/>
+
+                  <Label text="Imagen 4" />
                   <Image src="~/assets/01Longitud.png" stretch="none" />
+
+                  <Label text="Imagen 5" />
+                  <Image src="~/assets/01Longitud" stretch="none" />
                   <Button text="Botón" @tap="onButtonTap" />
 
+                  <Label text="Imagen 6" />
                   <Image src="~/assets/01Longitud.png" stretch="none" />
+
+                  <Label text="Imagen 7" />
                   <Image src="~/assets/00prueba.png" stretch="none" />
-                  <Image src="https://i.etsystatic.com/6792824/r/il/4cd9bd/4246801182/il_794xN.4246801182_d2z8.jpg" />
-                  <Label text="Esto no va 02" />
+
+                  <Label text="Imagen 8" />
+                  <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" />
+
+                  <Label text="Imagen 9" />
                   <Image src="~/assets/00prueba.png" stretch="none" />
-                  <Image src="https://i.etsystatic.com/6792824/r/il/4cd9bd/4246801182/il_794xN.4246801182_d2z8.jpg" stretch="none" />
+
+                  <Label text="Imagen 10" />
+                  <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" stretch="none" />
               </StackLayout>
         </ScrollView>
     </Page>
 </template>
 
 <script>
+
+
   export default {
     computed: {
       message() {
